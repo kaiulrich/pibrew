fc5fbc290a2c2cd38a8f39c09d90221e264b531b
Kai Ulrich <kaiu@gmx.de>
2016-11-24 10:43:27 +0100

    Initial page flow

--- pibrew.py
+++ pibrew.py
@@ -2,53 +2,34 @@
 
 from os import system
 import curses
-
-def get_param(prompt_string):
-     screen.clear()
-     screen.border(0)
-     screen.addstr(2, 2, prompt_string)
-     screen.refresh()
-     input = screen.getstr(10, 10, 60)
-     return input
-
-def execute_cmd(cmd_string):
-     system("clear")
-     a = system(cmd_string)
-     print ""
-     if a == 0:
-          print "Command executed correctly"
-     else:
-          print "Command terminated with error"
-     raw_input("Press enter")
-     print ""
+import configparser
 
 x = 0
 
-def run_recept:
+def run_recept():
+     config = configparser.ConfigParser()
+     config.read('pibrew.ini')
+     recipe = config['Main']['recipe']
+     screen.clear()
+     screen.border(0)
+     screen.addstr(2, 2, "Pi-Brew")
+     screen.addstr(4, 2, "Running " + recipe + ". Please enter a '0' to exit")
+     screen.refresh()
+
+     x = screen.getch()
+     
      while x != ord('0'):
-          screen.clear()
-          screen.border(0)
-          screen.addstr(2, 2, "Pi-Brew")
-          screen.addstr(4, 2, "Please enter a number...")
-          screen.addstr(6, 4, "1 - Run recipe")
-          screen.addstr(7, 4, "2 - Re-run recipe")
-          screen.addstr(9, 4, "3 - Exit")
+          x = screen.getch()
           screen.refresh()
 
-	     x = screen.getch()
-
-	     if x == ord('1'):
-		  username = get_param("Enter the username")
-		  curses.endwin()
-		  execute_cmd("echo 'Start rezept: " + username + "'")
-	     if x == ord('2'):
-		  curses.endwin()
-		  execute_cmd("echo 'Restart running rezept'")
 
 
 
 while x != ord('3'):
      screen = curses.initscr()
+     curses.cbreak()
+     curses.noecho()
+     curses.curs_set(0)
 
      screen.clear()
      screen.border(0)
@@ -62,12 +43,11 @@ while x != ord('3'):
      x = screen.getch()
 
      if x == ord('1'):
-          username = get_param("Enter the username")
-          curses.endwin()
-          execute_cmd("echo 'Start rezept: " + username + "'")
+          run_recept()
+
      if x == ord('2'):
-          curses.endwin()
-          execute_cmd("echo 'Restart running rezept'")
+          run_recept()
+
     
 
 curses.endwin()
