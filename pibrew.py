90eb6577281cab6d16a0f81ac98bbedbadda63f2
Kai Ulrich <kaiu@gmx.de>
2016-11-24 11:49:19 +0100

    Show config parameters

--- pibrew.py
+++ pibrew.py
@@ -4,27 +4,51 @@ from os import system
 import curses
 import configparser
 
-x = 0
+
 
 def run_recept():
      config = configparser.ConfigParser()
      config.read('pibrew.ini')
      recipe = config['Main']['recipe']
-     screen.clear()
-     screen.border(0)
-     screen.addstr(2, 2, "Pi-Brew")
-     screen.addstr(4, 2, "Running " + recipe + ". Please enter a '0' to exit")
-     screen.refresh()
-
-     x = screen.getch()
+     y = 0
      
-     while x != ord('0'):
-          x = screen.getch()
+     while y != ord('0'):
+          screen.clear()
+          screen.border(0)
+          screen.addstr(2, 2, "Pi-Brew")
+          screen.addstr(4, 2, "Running " + recipe)
+          phase_num = 0
+          for phase in config.sections():
+                if phase != 'Main':
+                     screen.addstr(8 + (phase_num * 2), 2, phase)
+                     screen.addstr(8 + (phase_num * 2), 30, config.get(phase, 'temp') + '°C')
+                     screen.addstr(8 + (phase_num * 2), 40, config.get(phase, 'time') + ' min')
+
+                     if config.get(phase, 'continue_manual') == '0':
+                          screen.addstr(8 + (phase_num * 2), 50, 'auto')
+                     else:
+                          screen.addstr(8 + (phase_num * 2), 50, 'manuel')
+
+                     if config.get(phase, 'action') == '0':
+                          screen.addstr(8 + (phase_num * 2), 60, 'No Action')
+                     elif config.get(phase, 'action') == '1':
+                           screen.addstr(8 + (phase_num * 2), 60, 'Alert')
+                     elif config.get(phase, 'action') == '2':
+                           screen.addstr(8 + (phase_num * 2), 60, 'End')
+                     else:
+                           screen.addstr(8 + (phase_num * 2), 50, config.get(phase, 'action'))
+
+
+                     phase_num = phase_num + 1
+
+          screen.addstr(8 + (phase_num * 2) + 2, 2, "Please enter '0' to exit")
+  
           screen.refresh()
+          y = screen.getch()
 
 
 
-
+x = 0
 while x != ord('3'):
      screen = curses.initscr()
      curses.cbreak()
@@ -47,6 +71,7 @@ while x != ord('3'):
 
      if x == ord('2'):
           run_recept()
+
 
     
 
