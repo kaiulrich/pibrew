a029b1bcaa3ad9621030ab87168709a55ee39018
Kai Ulrich <kaiu@gmx.de>
2016-12-08 13:59:54 +0100

    Real Modules added

---
+++ DS18B20Termometer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+class DS18B20Termometer:
+      def __init__(self):
+          self.up = 1
+          self.phase_reached = 0
+          self.sensor_temp = 43.0
+          self.phase_temp = 0.0         
+
+      def start(self, phase_temp):
+           self.up = 1
+           self.phase_reached = 0
+           #self.sensor_temp = 43.0
+           self.phase_temp = phase_temp
+           
+
+      def stop(self):
+           self.up = 0
+           self.phase_reached = 0
+           self.sensor_temp = 0
+           self.phase_temp = 0
+
+
+      def read_sensor_temp(self):
+           self.sensor_temp = self.sensor_temp + self.up
+   
+           if(not self.phase_reached and self.sensor_temp >= self.phase_temp):
+                self.phase_reached = 1
+
+      def temp_div(self):
+           return self.sensor_temp - self.phase_temp
+
+
+
+      def goUp(self):
+           self.up = 1
+
+      def goDown(self):
+           self.up = -1
+
+      def doStay(self):
+           self.up = 0
+
+
+      def get_phase_reached(self):
+           return self.phase_reached
+
+      def get_sensor_temp(self):
+           return self.sensor_temp
+
+      def get_phase_temp(self):
+           return self.phase_temp
+
+      def get_up(self):
+           return self.up
---
+++ RealHeater.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class SimHeater:
+      def __init__(self):
+           self.heating = 0
+
+      def heater_on(self):
+           self.heating = 1
+
+      def heater_off(self):
+          self.heating = 0
+
+      def heater_toggle(self):
+          self.heating = not self.heating
+
+      def is_heating(self):
+           return self.heating
